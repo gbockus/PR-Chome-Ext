def36e37d10101dce1bd27ff5105d2d621516ebc
gbockus <gbockus@spanning.com>
2016-05-26 15:57:42 -0500

    commiting for machine move

--- src/background.js
+++ src/background.js
@@ -1,2 +1,70 @@
 console.log('In background.js');
 console.log(config);
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  'use strict';
+  if (request.getRepos) {
+    let usernamePromise = getChromeStorage(config.githubUserName),
+      passwordPromise = getChromeStorage(config.githubPassword);
+
+    Promise.all([usernamePromise, passwordPromise])
+      .then((results) => {
+        let username = results[0];
+        let password = results[1];
+
+        console.log(username);
+        console.log(password);
+        // basic auth
+        const gh = new GitHub({
+          username: username,
+          password: password
+        });
+        
+        let user = gh.getUser();
+
+        let userRepoProm = user.listRepos()
+          .then((repos) => {
+            console.log(repos);
+            return repos.data;
+            // setChromeStorage(config.repositories, repos.data);
+          });
+        let orgRepoProm = user.listOrgs()
+          .then((orgs) => {
+            orgs = orgs.data;
+            let orgReposPromsies = [orgs];
+            orgs.forEach((org) => {
+              orgReposPromsies.push(gh.getOrganization(org.login).getRepos());
+            });
+            return Promise.all(orgReposPromsies);
+          })
+          .then((orgRepos) => {
+            let orgs = orgRepos[0];
+            let orgStorageObj = {};
+            let orgList = [];
+            orgs.forEach((org, i) => {
+              orgList.push({
+                login: org.login,
+                repos: orgRepos[i+1].data
+              })
+            });
+            console.log(orgList);
+            return orgList;
+            // setChromeStorage(config.orgs, orgList);
+          });
+
+        return Promise.all([userRepoProm, orgRepoProm]);
+      })
+      .then(([personalRepos = [], orgList]) => {
+        if (personalRepos.length > 0) {
+          orgList.push({login: 'Personal', repos: personalRepos});
+        }
+        console.log(orgList, 'final org list');
+        return setChromeStorage(config.repositories, orgList);
+      })
+      .then(() => {
+        chrome.runtime.sendMessage({reposLoaded: true});
+      });
+
+
+  }
+});
--- src/config.js
+++ src/config.js
@@ -1,7 +1,24 @@
-let storageKey = 'gh-pr-mon';
+const storageKey = 'gh-pr-mon';
 const config = {
   githubApiKey: `${storageKey}-gh-api-key`,
   githubUserName: `${storageKey}-gh-username`,
   githubPassword: `${storageKey}-gp-password`,
-  repositories: `${storageKey}-repos`
+  repositories: `${storageKey}-repos`,
+  orgs: `${storageKey}-orgs`,
+  selectedRepos: `${storageKey}-selected-repos`
 };
+
+function getChromeStorage(key) {
+  return new Promise((resolve) => {
+    chrome.storage.local.get(key, (obj) => {
+      resolve(obj[key]);
+    });
+  });
+}
+
+
+function setChromeStorage(key, value) {
+  const obj = {};
+  obj[key] = value;
+  chrome.storage.local.set(obj);
+}
